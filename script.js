e6249fbda336071198d811c724ed23381e6eb41b
Erf <105368292+DeviceIR@users.noreply.github.com>
2023-06-20 14:22:36 +0330

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const content = document.querySelector(".content");
 timestamps.unshift(getTimestamp());
 
 let wpmNumber = 0;
-let time_limit = 5;
+let time_limit = 10;
 let sum = 0;
 let counterChar = 0;
 let counterWord = 0;
@@ -146,6 +146,7 @@ function getTimestamp() {
 }
 
 function setContent() {
+  content.classList.add('.percent');
   content.textContent = null;
   current_content =
     "The bikers rode down the long and narrow path to reach the city park. When they reached a good spot to rest, they began to look for signs of spring. The sun was bright, and a lot of bright red and blue blooms proved to all that warm spring days were the very best. Spring rides were planned. They had a burger at the lake and then rode farther up the mountain. As one rider started to get off his bike, he slipped and fell. One of the other bikers saw him fall but could do nothing to help him. Neither the boy nor the bike got hurt. After a brief stop, everyone was ready to go on. All the bikers enjoyed the nice view when they came to the top. All the roads far below them looked like ribbons. A dozen or so boats could be seen on the lake. It was very quiet and peaceful and no one wished to leave. As they set out on their return, they all enjoyed the ease of pedaling. The bikers came upon a new bike trail. This route led to scenery far grander than that seen from the normal path. The end of the day brought laughs and cheers from everyone. The fact that each person was very, very tired did not keep anyone from eagerly planning for the exciting ride to come.";
